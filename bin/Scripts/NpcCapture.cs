b2e52159eb01509e0017d0e394b44adffc49e95a
Maple <8961085+MapleHinata@users.noreply.github.com>
2023-05-29 22:34:25 -0300

    Fix NpcCapture.cs script and add RsvDump.cs script

--- bin/Scripts/NpcCapture.cs
+++ bin/Scripts/NpcCapture.cs
@@ -28,9 +28,9 @@ if(Packet.Name == "NpcSpawn")
 	}
 
 	var entId = BitConverter.ToUInt32(Packet.Data, 4);
-    //        public static string GetExdField(string sheetName, int row, int col)
+	var exdData = new FFXIVMonReborn.ExdDataCache("D:/SteamLibrary/steamapps/common/FINAL FANTASY XIV Online/game/sqpack");
 
-    var name = FFXIVMonReborn.ExdReader.GetExdFieldAsString("BNpcName", (int)parsed.bNPCName, "Singular");
+    var name = exdData.GetBnpcName(parsed.bNPCName);
     
     Debug.WriteLine($"Spawn packet: {entId} Base: {parsed.bNPCBase} Name: {name}");
 
